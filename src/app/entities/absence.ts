8cce77e7a042f3440527cd8094535ad9fbb59172
thewickedgoat <noahbock@live.dk>
2017-11-11 20:04:56 +0100

    added properties to the status enum and absence entitiy

--- src/app/entities/absence.ts
+++ src/app/entities/absence.ts
@@ -1,7 +1,8 @@
-import {User} from "./user";
+import {User} from './user';
+import {Status} from 'tslint/lib/runner';
 
 export class Absence {
   user: User;
   date: Date;
-
+  status: Status;
 }
---
+++ src/app/entities/status.enum.ts
@@ -0,0 +1,15 @@
+export enum Status {
+  S = 1,
+  HS = 2,
+  F = 3,
+  HF = 4,
+  FF = 5,
+  HFF = 6,
+  K = 7,
+  B = 8,
+  BS = 9,
+  AF = 10,
+  A = 11,
+  HA = 12,
+  SN = 13
+}
